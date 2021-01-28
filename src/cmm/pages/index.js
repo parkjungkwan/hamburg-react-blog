5f5936c1b72c489d989f6bf0524d0d665b3e68bb
parkjungkwan <pakjkwan@gmail.com>
2021-01-28 16:55:24 +0900

    16

---
+++ src/cmm/pages/index.js
@@ -0,0 +1,9 @@
+export { MainPage } from './MainPage'
+export { Blog } from './Blog'
+export { FeaturedPost } from './FeaturedPost'
+export { Footer } from './Footer'
+export { Header } from './Header'
+export { MainFeaturedPost } from './MainFeaturedPost'
+export { Markdown } from './Markdown'
+export { Sidebar } from './Sidebar'
+
---
+++ src/root.reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import { userReducer } from './uss/modules/index'
+
+export const rootReducer = combineReducers({
+    userReducer
+   
+})
+export default rootReducer
